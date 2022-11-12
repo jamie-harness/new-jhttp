178fb5fa6dd6b8a8b1a294589dab1211e2ce271b
Jamie Li <tianyi.li@harness.io>
2022-11-12 12:38:05 -0500

    always-run

---
+++ src/test/java/io/harness/agent/sdk/HarnessAlwaysRun.java
@@ -0,0 +1,7 @@
+package io.harness.agent.sdk;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HarnessAlwaysRun { }
